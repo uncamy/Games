e1d0c3581e9c0027e593736ce43e8c120953281c
Amy Roberts <aroberts@live.unc.edu>
2013-08-01 16:02:39 -0400

    Calculate grades for class

---
+++ classStats.py
@@ -0,0 +1,26 @@
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+def print_grades(grades):
+    for grade in grades:
+        print grade
+
+def grades_sum(grades):
+    total = 0
+    for grade in grades: 
+        total += grade
+    return total
+    
+def grades_average(grades):
+    sum_of_grades = grades_sum(grades)
+    average = sum_of_grades / len(grades)
+    return average
+
+def grades_variance(grades, average):
+    variance = 0
+    for i in grades:
+        diff_sq = (average - i)**2
+        variance += diff_sq
+    return variance/ len(grades)
+
+print grades_variance(grades, grades_average(grades))
+
